dbb9ba44027413bb09d708f82ea46d4b1500a611
Luca Campanella <luca.campanella@aspag.it>
2024-10-17 17:28:07 +0200

    domande suddivise in sezioni

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,55 @@
 import { QUESTIONS } from "./data.js";
 
-let questionForm = document.getElementById("question-form")
+//Caricamento Modal
+document.addEventListener("DOMContentLoaded", function () {
+  var modal = new bootstrap.Modal(document.getElementById("modal"), {
+    backdrop: "static",
+    keyboard: false,
+  });
+  modal.show();
+});
+
+let questionForm = document.getElementById("question-form");
+let currentSectionIndex = 0;
+
+let sections = new Set();
+QUESTIONS.forEach((question) => {
+  let firstLetter = question.id.charAt(0);
+  sections.add(firstLetter);
+});
+
+sections = Array.from(sections);
 
 //crea i radio button per ogni domanda
-function createSixRadioButtons(questionId) {
-    let radioHTML = '';
-    
-    // Genera 6 radio button per ogni domanda
-    for (let i = 1; i <= 6; i++) {
-        radioHTML += `
+function createRadioButtons(questionId, index) {
+  let radioHTML = "";
+
+  QUESTIONS[index].options.forEach((option, optionIndex) => {
+    radioHTML += `
             <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="question${questionId}" id="inlineRadio${i}" value="${i}">
-                <label class="form-check-label radio-label" for="inlineRadio${i}">${i}</label>
+                <input class="form-check-input" type="radio" name="question${questionId}" id="${questionId}_${optionIndex}" value="${option}">
+                <label class="form-check-label personal-data-label" for="${questionId}_${optionIndex}">${option}</label>
             </div>
         `;
-    }
+  });
 
-    return radioHTML;
+  return radioHTML;
 }
 
+//Genera tutte le domande a partire dall'array QUESTIONS
 function createCards() {
-    let fullHTML = '';
+  let fullHTML = "";
 
-    for (let i = 0; i < QUESTIONS.length; i++) {
-        let questionHTML = `
-        <div class="row mb-5">
+  for (let i = 0; i < QUESTIONS.length; i++) {
+    let questionHTML = `
+        <div class="row mb-5 question-row" data-id="${QUESTIONS[i].id}">
             <div class="col-sm-2"></div>
             <div class="col-lg-8">
                 <div class="card">
-                    <h5 class="card-header">${QUESTIONS[i].testo}</h5>
+                    <h5 class="card-header">${QUESTIONS[i].text}</h5>
                     <div class="card-body">
                         <div class="w-100 d-flex flex-wrap justify-content-around">
-                            ${createSixRadioButtons(QUESTIONS[i].id)}
+                            ${createRadioButtons(QUESTIONS[i].id, i)}
                         </div>
                     </div>
                 </div>
@@ -39,25 +57,51 @@ function createCards() {
             <div class="col-sm-2"></div>
         </div>
         `;
-        fullHTML += questionHTML; 
-    }
-
-    questionForm.innerHTML = fullHTML; 
+    fullHTML += questionHTML;
+  }
+  questionForm.innerHTML = fullHTML;
 }
 
-createCards()
+createCards();
 
-document.addEventListener("DOMContentLoaded", function() {
-    var modal = new bootstrap.Modal(document.getElementById('modal'));  // Inizializza la modal
-    modal.show();  // Mostra la modal
+//Mostra una determinata sezione
+function showSection(letter) {
+  const allQuestions = document.querySelectorAll(".question-row");
+  allQuestions.forEach((question) => {
+    question.style.display = "none";
+  });
+
+  const filteredQuestions = document.querySelectorAll(
+    `.question-row[data-id^="${letter}"]`
+  );
+  filteredQuestions.forEach((question) => {
+    question.style.display = "flex";
+  });
+
+  updateButtons();
+}
+
+//Abilita il pulsante indietro nella prima sezione e avanti nell'ultima
+function updateButtons() {
+  document.getElementById("btn-indietro").disabled = currentSectionIndex === 0;
+  document.getElementById("btn-avanti").disabled =
+    currentSectionIndex === sections.length - 1;
+}
+
+//Event listener Btn avanti
+document.getElementById("btn-avanti").addEventListener("click", function () {
+  if (currentSectionIndex < sections.length - 1) {
+    currentSectionIndex++;
+    showSection(sections[currentSectionIndex]);
+  }
+});
+//Event listener Btn indietro
+document.getElementById("btn-indietro").addEventListener("click", function () {
+  if (currentSectionIndex > 0) {
+    currentSectionIndex--;
+    showSection(sections[currentSectionIndex]);
+  }
 });
 
-// CONSOLE LOG ON SUBMIT
-function onSubmit(){
-   const ANSWERS = document.querySelectorAll('input');
-   ANSWERS.forEach((answer) =>{
-    if(answer.checked) {
-        console.log(answer.name + ' ' + answer.value)
-    }
-   })
-}
+// Mostra la prima sezione all'avvio
+showSection(sections[currentSectionIndex]);
